98721b7bdfe719c4fa59b6c0439166501b4d238e
Andrew <andresgallegod@gmail.com>
2023-11-13 16:14:59 -0500

    Reinicio de todo para el repositorio

---
+++ suma.js
@@ -0,0 +1,17 @@
+var a = parseFloat(prompt("Ingrese el primer numero a sumar: "))
+
+var b = parseFloat(prompt("Ingrese el segundo numero a sumar: "))
+
+var Suma = a + b
+var Resta = a - b
+var Multiplicacion = a * b
+var Division = a / b
+
+
+document.write("El resultado de la suma entre "+a+" y "+b+" es: "+Suma)
+
+document.write("El resultado de la resta entre "+a+" y "+b+" es: "+Resta)
+
+document.write("El resultado de la multiplicacion entre "+a+" y "+b+" es: "+Multiplicacion)
+
+document.write("El resultado de la division entre "+a+" y "+b+" es: "+Division)
